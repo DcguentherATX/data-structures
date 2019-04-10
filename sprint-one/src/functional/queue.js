2697d9904a43f14fb35e2ccb3ebbe05bc9d4a7fe
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-10 15:21:03 -0500

    functional/queue.js complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,33 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  var count = 0;
+  someInstance.enqueue = function(value) {   
+    storage[count] = value;
+    count = count + 1; 
   };
 
   someInstance.dequeue = function() {
+    var numArr = [];
+    var keys = Object.keys(storage);
+
+    for (var i = 0; i < keys.length; i++) {
+        numArr.push(Number(keys[i]));
+    }
+    var front = Math.min(...numArr);
+    var returnValue = storage[front];
+    delete storage[front];
+    return returnValue;
+  
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
 };
+
+var q1 = Queue()
+q1.enqueue('a');
