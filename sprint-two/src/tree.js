034a7ae06933dc906b72f3bf732e20abd9a1e3d2
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-11 20:03:20 -0500

    tree half done

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  
 
   return newTree;
 };
@@ -11,9 +12,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+
+  
+  var checkChildren = function(tree, target) {
+    for (var i = 0; i < tree.children.length; i++) {
+      var child = tree.children[i];      
+      if (child === target) {
+        return true;
+      }
+      else if (child.children.length === 0) {
+        return false;    
+      }
+      else if (child.children.length > 0) {
+        return checkChildren(child, target);
+      }
+    }
+  }
+  if (this.value === target) {
+    return true;
+  } else {
+    return checkChildren(this, target);
+  }
 };
 
 
