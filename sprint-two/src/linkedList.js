5f54a4d21d7036af428752f9a4ccdffba09cb270
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-11 19:14:25 -0500

    linkedList fixed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,62 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }    
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var oldHead = list.head.value;
+      list.head = list.head.next;
+      return oldHead;
+    }
   };
 
+  // list.contains = function(target) {
+  //   if(list.head.value === target) {
+  //     return true;
+  //   } else if (list.head.value !== null) {
+  //     var nextNode = list.head.next;
+       
+  //     var checkNodes = function(nextNode) {
+  //       if(nextNode.next === null) {
+  //         return false;
+  //       }
+  //       if(nextNode.value === target) {
+  //         return true;
+  //       }
+  //       if(nodeNext.next !== null) {
+  //         return checkNodes(nextNode.next);
+  //       }
+  //     }
+  //     checkNodes(nextNode);
+  //   } else {
+  //     return false;
+  //   }
+  // };
+
   list.contains = function(target) {
+
+    var checkNodes = function(node) {
+      if (node.next === null && node.value !== target) {
+        return false;
+      }
+      else if (node.value === target) {
+        return true;
+      }
+      else if (node.next !== null) {
+        return checkNodes(node.next);
+      }
+    };
+    return checkNodes(this.head);
   };
 
   return list;
@@ -23,7 +73,7 @@ var Node = function(value) {
 
   return node;
 };
-
+   
 /*
  * Complexity: What is the time complexity of the above functions?
  */
