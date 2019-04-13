b2723dab6a6ec3f6e1013049db9ba4c5bef6bb7a
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-13 12:06:58 -0500

    binary tree complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,77 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(BinarySearchTree.prototype);  
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  //create new tree;
+  var newTree = BinarySearchTree(value);
+
+  var binarySearch = function(tree) {
+  //if tree value is less than root value move left
+    if (newTree.value < tree.value) {
+      if(tree.left === null) {
+        tree.left = newTree; 
+      } else {
+        binarySearch(tree.left);
+      }
+    }
+    if (newTree.value > tree.value) { 
+      if(tree.right === null) {
+        tree.right = newTree;
+      } else if (tree.right !== null) {
+        binarySearch(tree.right);
+      }
+    }    
+  //if greater than move right
+  //repeat process until find a left/right prop that is null  
+  }
+  binarySearch(this);
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+  var valueSearch = function(tree) {
+  //if tree value is less than root value move left
+
+    if(value === tree.value) {
+      return true;    
+    }
+    else if (value < tree.value && tree.left !== null) {
+      return valueSearch(tree.left);
+    }
+    else if (value > tree.value && tree.right !== null) { 
+      return valueSearch(tree.right);
+    }
+    else {
+      return false;
+    }
+  };    
+  //if greater than move right
+  //repeat process until find a left/right prop that is null  
+  return valueSearch(this);
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+  var iterate = function(tree) {
+    cb(tree.value);
+
+    if (tree.left !== null) {
+      iterate(tree.left);
+    }
+    if (tree.right !== null) {
+      iterate(tree.right);
+    }
+  }
+
+  iterate(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
